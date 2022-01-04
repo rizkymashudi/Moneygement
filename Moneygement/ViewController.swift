500b1a1dff2125cd5693a5ac84da52a313e1d05a
rimas12 <rizky_mashudy@yahoo.co.id>
2022-01-04 11:44:03 +0700

    Initial Commit

---
+++ Moneygement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moneygement
+//
+//  Created by Rizky Mashudi on 04/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
